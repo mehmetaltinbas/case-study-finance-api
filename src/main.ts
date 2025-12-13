5b9d73020630380b758e794d72cab03a325a3e2f
altnbsmehmet <altnbsmehmet@icloud.com>
2025-12-13 19:13:10 +0300

    docker deploy pipeline

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
 import cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
     const app = await NestFactory.create(AppModule);
@@ -22,6 +22,5 @@ async function bootstrap(): Promise<void> {
         credentials: true,
     });
     await app.listen(port);
-    console.log(`Server is running on http://localhost:${port}\n`);
 }
 bootstrap().catch(e => console.log(e));
